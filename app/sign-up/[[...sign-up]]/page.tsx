a64570000bf028ecb9e72f29a2d587e30227f3c1
elisa <shemaelyssa@gmail.com>
2024-12-30 17:50:11 +0200

    changes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,38 +1,213 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import * as Clerk from "@clerk/elements/common";
 import * as SignUp from "@clerk/elements/sign-up";
+import { Github, Loader2 } from "lucide-react";
+import Link from "next/link";
 
 export default function SignUpPage() {
   return (
-    <SignUp.Root>
-      <SignUp.Step name="start">
-        <h1>Create an account</h1>
+    <div className="grid w-full grow items-center px-4 sm:justify-center mt-20">
+      <SignUp.Root>
+        <Clerk.Loading>
+          {(isGlobalLoading) => (
+            <>
+              <SignUp.Step name="start">
+                <Card className="w-full sm:w-96">
+                  <CardHeader>
+                    <CardTitle>Sign up To Revalidate</CardTitle>
+                    <CardDescription>
+                      Welcome ! Please create Account to continue
+                    </CardDescription>
+                  </CardHeader>
 
-        <Clerk.Connection name="google">Sign up with Google</Clerk.Connection>
+                  <CardContent className=" grid gap-y-2">
+                    <Clerk.Field name="identifier">
+                      <Clerk.Label asChild>
+                        <Label>Email address</Label>
+                      </Clerk.Label>
+                      <Clerk.Input type="email" required asChild>
+                        <Input placeholder="Enter email address" />
+                      </Clerk.Input>
+                      <Clerk.FieldError className=" block text-sm text-destructive" />
+                    </Clerk.Field>
+                    {/* <SignUp.Captcha /> */}
+                    <div className="grid w-full">
+                      <SignUp.Action submit asChild name="continue">
+                        <Button disabled={isGlobalLoading}>
+                          <Clerk.Loading>
+                            {(isLoading) =>
+                              isLoading ? (
+                                <Loader2 className="size-4 animate-spin" />
+                              ) : (
+                                "Continue"
+                              )
+                            }
+                          </Clerk.Loading>
+                        </Button>
+                      </SignUp.Action>
+                    </div>
+                  </CardContent>
+                  <Separator />
+                  <span className="flex justify-center items-center font-bold mt-2">
+                    Or continue With
+                  </span>
 
-        <Clerk.Field name="identifier">
-          <Clerk.Label>Email</Clerk.Label>
-          <Clerk.Input />
-          <Clerk.FieldError />
-        </Clerk.Field>
+                  <CardFooter className="grid grid-cols-2 py-2 gap-x-2">
+                    <Clerk.Connection name="github" asChild>
+                      <Button
+                        size="sm"
+                        variant="outline"
+                        type="button"
+                        disabled={isGlobalLoading}
+                      >
+                        <Clerk.Loading scope="provider:github">
+                          {(isLoading) =>
+                            isLoading ? (
+                              <Loader2 className="size-4 animate-spin" />
+                            ) : (
+                              <>
+                                <Github className="mr-2 size-4" />
+                                GitHub
+                              </>
+                            )
+                          }
+                        </Clerk.Loading>
+                      </Button>
+                    </Clerk.Connection>
+                    <Clerk.Connection name="google" asChild>
+                      <Button
+                        size="sm"
+                        variant="outline"
+                        type="button"
+                        disabled={isGlobalLoading}
+                      >
+                        <Clerk.Loading scope="provider:google">
+                          {(isLoading) =>
+                            isLoading ? (
+                              <Loader2 className="size-4 animate-spin" />
+                            ) : (
+                              <>
+                                <svg
+                                  className="mr-2 h-4 w-4"
+                                  aria-hidden="true"
+                                  focusable="false"
+                                  data-prefix="fab"
+                                  data-icon="google"
+                                  role="img"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 488 512"
+                                >
+                                  <path
+                                    fill="currentColor"
+                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                                  ></path>
+                                </svg>
+                                Google
+                              </>
+                            )
+                          }
+                        </Clerk.Loading>
+                      </Button>
+                    </Clerk.Connection>
+                    <Button variant="link" size="sm" asChild>
+                      <Link href="/sign-in">
+                        Already have an account? Sign in
+                      </Link>
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </SignUp.Step>
+              <SignUp.Step name="continue">
+                  <h4>COntinue</h4>
+              </SignUp.Step>
+              <SignUp.Step name="verifications">
+                <SignUp.Strategy name="email_code">
+                  <Card className="w-full sm:w-96">
+                    <CardHeader>
+                      <CardTitle>Check Your Email</CardTitle>
+                      <CardDescription>
+                        Enter the verification code sent to your email
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent className=" grid gap-y-4">
+                      <Clerk.Field name="code">
+                        <Clerk.Label asChild>
+                          <Label>Email Verification code</Label>
+                        </Clerk.Label>
+                        <div className="grid items-center justify-center gap-y-2">
+                          <div className="flex justify-center text-center">
+                            <Clerk.Input
+                              type="otp"
+                              autoSubmit
+                              className="flex justify-center has-[:disabled]:opacity-50"
+                              render={({ value, status }) => (
+                                <div
+                                  data-status={status}
+                                  className="relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md data-[status=cursor]:ring-1 data-[status=selected]:ring-1 data-[status=cursor]:ring-ring data-[status=selected]:ring-ring"
+                                >
+                                  {value}
+                                </div>
+                              )}
+                            />
+                          </div>
+                        </div>
 
-        <SignUp.Action submit>Sign up</SignUp.Action>
-      </SignUp.Step>
+                        <Clerk.FieldError className="block text-center text-sm text-destructive" />
+                      </Clerk.Field>
 
-      <SignUp.Step name="verifications">
-        <SignUp.Strategy name="email_code">
-          <h1>Check your email</h1>
-
-          <Clerk.Field name="code">
-            <Clerk.Label>Email Code</Clerk.Label>
-            <Clerk.Input />
-            <Clerk.FieldError />
-          </Clerk.Field>
-
-          <SignUp.Action submit>Verify</SignUp.Action>
-        </SignUp.Strategy>
-      </SignUp.Step>
-    </SignUp.Root>
+                      <SignUp.Action
+                        resend
+                        className="text-muted-foreground"
+                        fallback={({ resendableAfter }) => (
+                          <Button variant="link" size="sm" disabled>
+                            Didn&apos;t recieve a code? Resend (
+                            <span className="tabular-nums">
+                              {resendableAfter}
+                            </span>
+                            )
+                          </Button>
+                        )}
+                      >
+                        <Button variant="link" size="sm">
+                          Didn&apos;t recieve a code? Resend
+                        </Button>
+                      </SignUp.Action>
+                    </CardContent>
+                    <CardFooter>
+                      <SignUp.Action submit asChild>
+                        <Button disabled={isGlobalLoading}>
+                          <Clerk.Loading>
+                            {(isLoading) => {
+                              return isLoading ? (
+                                <Loader2 className="size-4 animate-spin" />
+                              ) : (
+                                "Continue"
+                              );
+                            }}
+                          </Clerk.Loading>
+                        </Button>
+                      </SignUp.Action>
+                    </CardFooter>
+                  </Card>
+                </SignUp.Strategy>
+              </SignUp.Step>
+            </>
+          )}
+        </Clerk.Loading>
+      </SignUp.Root>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,10 +20,7 @@ export default {
   				DEFAULT: 'hsl(var(--popover))',
   				foreground: 'hsl(var(--popover-foreground))'
   			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
+  			primary:'var(--primary)',
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
   				foreground: 'hsl(var(--secondary-foreground))'
